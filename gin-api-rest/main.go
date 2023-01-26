3d5199738cc1e76819cfb8b1b466dcaa5a03da40
Vinicius Teixeira <99350364+viniciushammett@users.noreply.github.com>
2023-01-25 22:15:01 -0300

    Uploading DB

--- gin-api-rest/main.go
+++ gin-api-rest/main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
+	database.ConectaComBancoDeDados()
 	models.Alunos = []models.Aluno{
 		{Nome: "Vini Teixeira", CPF: "00000000000", RG: "1234657912"},
 		{Nome: "Vini Santos", CPF: "00000000000", RG: "1234657914"},
