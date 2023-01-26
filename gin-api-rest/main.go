8875a7f99f40cd449ff09ae89198b14d6d62bb4e
Vinicius Teixeira <99350364+viniciushammett@users.noreply.github.com>
2023-01-26 20:53:49 -0300

    Updating parameters

--- gin-api-rest/main.go
+++ gin-api-rest/main.go
@@ -7,9 +7,5 @@ import (
 
 func main() {
 	database.ConectaComBancoDeDados()
-	models.Alunos = []models.Aluno{
-		{Nome: "Vini Teixeira", CPF: "00000000000", RG: "1234657912"},
-		{Nome: "Vini Santos", CPF: "00000000000", RG: "1234657914"},
-	}
 	routes.HandleRequests()
 }
