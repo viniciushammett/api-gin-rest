98da969209df8e6fe57bd74e9ce361757ac5ad77
Vinicius Teixeira <99350364+viniciushammett@users.noreply.github.com>
2023-01-26 22:27:46 -0300

    adding validation

--- gin-api-rest/models/aluno.go
+++ gin-api-rest/models/aluno.go
@@ -1,10 +1,22 @@
 package models
 
-import "gorm.io/gorm"
+import ( 
+	"gorm.io/gorm" 
+	"gopkg.in/validator.v2"
+	)
+
 
 type Aluno struct {
 	gorm.Model
-	Nome string `json:"nome"`
-	CPF  string `json:"cpf"`
-	RG   string `json:"rg"`
+	Nome string `json:"nome validate:"nonzero"`
+	CPF  string `json:"cpf validate:"len=9"`
+	RG   string `json:"rg"validate:"len=11"`
+}
+
+func ValidaDadosDeAluno(aluno *Aluno) error {
+	if err := validator.Validate(aluno); err != nil {
+		return err
+	}
+
+	return nil
 }
